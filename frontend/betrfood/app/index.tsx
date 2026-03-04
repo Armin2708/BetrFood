0e0a2d874fe44d9a7be0dfdd193d0f399cd68a96
hannahng626 <vyhannah626@gmail.com>
2026-03-04 15:12:12 -0800

    test

--- frontend/betrfood/app/index.tsx
+++ frontend/betrfood/app/index.tsx
@@ -1,14 +1,9 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home is rendering ✅</Text>
     </View>
   );
 }
