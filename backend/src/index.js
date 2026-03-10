1bb58cad09ed6c2a8530d9e7883ba5be67ff10a3
xuanhoa0322 <xuanhoa0322@gmail.com>
2026-03-10 09:24:13 -0700

    Add user and tags routes to the API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const tagsRoutes = require("./routes/tags");
+const userRoutes = require("./routes/users");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +10,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "BetrFood API" });
 });
+
+app.use("/api/tags", tagsRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
